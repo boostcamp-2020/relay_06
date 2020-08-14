9cb67de41499dbc9d33a04e993813b6855eeaa12
Changehee Choi <changhee.dev@gmail.com>
2020-08-14 19:42:34 +0900

    PhotoPageImage 컴포넌트 삭제

--- frontend/src/pages/GalleryPhotoPage.js
+++ frontend/src/pages/GalleryPhotoPage.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from '../components/Navbar';
 // import './GalleryPhotoPage.css';
-import { Jumbotron } from 'react-bootstrap';
-import PhotoPageImage from '../components/PhotoPageImage';
+import { Jumbotron, Image } from 'react-bootstrap';
+import * as api from '../lib/image-api';
 
 /* src= "image.png" 에 이미지 링크 추가하면 될 것 같음 */
 function GalleryPhotoPage({ match }) {
   // const tags = _tags;
   // const imageUrl = _imageUrl;
   // const time = _time;
+
+  const [imageInfo, setImageInfo] = useState({});
+
+  useEffect(() => {
+    const imageId = match.params.id;
+    api.getImage(imageId).then((imageInfo) => setImageInfo(imageInfo));
+  });
 
   // test data
   let tags = ['집', '개', '고양이'];
@@ -28,7 +35,7 @@ function GalleryPhotoPage({ match }) {
         </div>
         <p> {time} </p>
         <div class="text-center">
-          <PhotoPageImage imageId={match.params.id} />
+          <Image src={imageInfo.url} fluid />
         </div>
       </Jumbotron>
     </>

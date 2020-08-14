b0ca8c8ba0b7e58c6a58205a94b8f77309948f82
j03y14 <j03y14@gmail.com>
2020-08-14 15:54:07 +0900

    라우터 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import PostEdit from "./components/PostEdit";
-
+import Routes from './components/Routes';
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <PostEdit />
+      <Routes/>
     </div>
   );
 }

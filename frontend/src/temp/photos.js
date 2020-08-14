61607ebcc9b1f15d2e75d6576533ae9b14e2ae0f
j03y14 <j03y14@gmail.com>
2020-08-14 16:03:14 +0900

    임시 데이터 수정

--- frontend/src/temp/photos.js
+++ frontend/src/temp/photos.js
@@ -1,18 +1,18 @@
 export const photos = [
     {
       src: "https://source.unsplash.com/2ShvY8Lf6l0/800x599",
-    //   width: 4,
-    //   height: 3
+      width: 4,
+      height: 3
     },
     {
       src: "https://source.unsplash.com/Dm-qxdynoEc/800x799",
-    //   width: 1,
-    //   height: 1
+      width: 1,
+      height: 1
     },
     {
       src: "https://source.unsplash.com/qDkso9nvCg0/600x799",
-    //   width: 3,
-    //   height: 4
+      width: 3,
+      height: 4
     },
     {
       src: "https://source.unsplash.com/iecJiKe_RNg/600x799",

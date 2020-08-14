adfc4950461bfd31400a14458f8ff794ba778249
Changehee Choi <changhee.dev@gmail.com>
2020-08-14 15:04:37 +0900

    feat: 라우터 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,40 @@
-import React from "react";
-import Navbar from "./components/Navbar";
-import PostEdit from "./components/PostEdit";
+// import React from "react";
+import Navbar from './components/Navbar';
+// import PostEdit from "./components/PostEdit";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <PostEdit />
-    </div>
-  );
+// function App() {
+//   return (
+
+/* <div className="App">
+  <Navbar />
+  <PostEdit />
+</div>; */
+
+//   );
+// }
+
+// export default App;
+
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Gallery, Home } from './pages';
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <BrowserRouter>
+          <div className="App">
+            <Navbar />
+            <div>
+              <Route exact path="/" component={Home} />
+              <Route path="/gallery" component={Gallery} />
+            </div>
+          </div>
+        </BrowserRouter>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;

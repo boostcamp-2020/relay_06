d84187db68f18be95e1785f9036f437bc7280069
YoonWoo <lkkjasd@korea.ac.kr>
2020-08-14 16:49:25 +0900

    Gallery페이지 사진리스트 띄우기

--- frontend/src/pages/Gallery.js
+++ frontend/src/pages/Gallery.js
@@ -1,5 +1,9 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
+import React from "react";
+import { Button } from "react-bootstrap";
+import Image from "react-bootstrap/Image";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const Gallery = () => {
   return (
@@ -8,6 +12,44 @@ const Gallery = () => {
       <Button variant="primary" href="/upload/image">
         사진등록
       </Button>
+      <p></p>
+      <Container>
+        <Row>
+          <Col xs={6} md={4}>
+            <Image src="https://i.imgur.com/Bo7B4AH.jpeg" thumbnail />
+          </Col>
+          <Col xs={6} md={4}>
+            <Image
+              src="https://i.pinimg.com/originals/cc/ab/ac/ccabacec8e84207f0b1a8a9d844049ec.gif"
+              thumbnail
+            />
+          </Col>
+          <Col xs={6} md={4}>
+            <Image
+              src="https://pgnqdrjultom1827145.cdn.ntruss.com/img/d6/3f/d63fc54819cd3fb0c319021e2e7cd6bfee951e8ce2db9e948bd828f538272da6_v1.jpg"
+              thumbnail
+            />
+          </Col>
+          <Col xs={6} md={3}>
+            <Image
+              src="https://image.freepik.com/free-photo/image-human-brain_99433-298.jpg"
+              thumbnail
+            />
+          </Col>
+          <Col xs={6} md={3}>
+            <Image src="https://i.imgur.com/Bo7B4AH.jpeg" thumbnail />
+          </Col>
+          <Col xs={6} md={3}>
+            <Image src="https://i.imgur.com/Bo7B4AH.jpeg" thumbnail />
+          </Col>
+          <Col xs={6} md={3}>
+            <Image src="https://i.imgur.com/Bo7B4AH.jpeg" thumbnail />
+          </Col>
+          <Col xs={6} md={3}>
+            <Image src="https://i.imgur.com/Bo7B4AH.jpeg" thumbnail />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };

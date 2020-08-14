7d2374d4a80f86d4f952d1951260b54ac5f361cc
YoonWoo <lkkjasd@korea.ac.kr>
2020-08-14 18:44:58 +0900

    Image Component Rendering

--- frontend/src/pages/Gallery.js
+++ frontend/src/pages/Gallery.js
@@ -12,44 +12,6 @@ const Gallery = () => {
       <Button variant="primary" href="/upload/image">
         사진등록
       </Button>
-      <p></p>
-      <Container>
-        <Row>
-          <Col xs={6} md={4}>
-            <Image src="https://i.imgur.com/Bo7B4AH.jpeg" thumbnail />
-          </Col>
-          <Col xs={6} md={4}>
-            <Image
-              src="https://i.pinimg.com/originals/cc/ab/ac/ccabacec8e84207f0b1a8a9d844049ec.gif"
-              thumbnail
-            />
-          </Col>
-          <Col xs={6} md={4}>
-            <Image
-              src="https://pgnqdrjultom1827145.cdn.ntruss.com/img/d6/3f/d63fc54819cd3fb0c319021e2e7cd6bfee951e8ce2db9e948bd828f538272da6_v1.jpg"
-              thumbnail
-            />
-          </Col>
-          <Col xs={6} md={3}>
-            <Image
-              src="https://image.freepik.com/free-photo/image-human-brain_99433-298.jpg"
-              thumbnail
-            />
-          </Col>
-          <Col xs={6} md={3}>
-            <Image src="https://i.imgur.com/Bo7B4AH.jpeg" thumbnail />
-          </Col>
-          <Col xs={6} md={3}>
-            <Image src="https://i.imgur.com/Bo7B4AH.jpeg" thumbnail />
-          </Col>
-          <Col xs={6} md={3}>
-            <Image src="https://i.imgur.com/Bo7B4AH.jpeg" thumbnail />
-          </Col>
-          <Col xs={6} md={3}>
-            <Image src="https://i.imgur.com/Bo7B4AH.jpeg" thumbnail />
-          </Col>
-        </Row>
-      </Container>
     </div>
   );
 };
--- frontend/src/pages/GalleryPage.js
+++ frontend/src/pages/GalleryPage.js
@@ -1,22 +1,67 @@
-import React from "react";
+import React, { Component } from "react";
 import Navbar from "../components/Navbar";
 import Gallery from "react-photo-gallery";
-import {photos} from '../temp/photos.js'
-import { Button } from 'react-bootstrap';
+import { photos } from "../temp/photos.js";
+import { Button, Image, Container, Row, Col } from "react-bootstrap";
+import { faPencilAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-
-
-function GalleryPage() {
+class GalleryPage extends React.Component {
+  render() {
     return (
-        <>
-            <Navbar/>
-            <Button variant="primary" href="/gallery/upload">
-                사진등록
-            </Button>
-            
-            <Gallery photos={photos} />
-        </>
+      // <>
+      <div>
+        <Navbar />
+        <Container>
+          <Button
+            style={{ float: "right" }}
+            variant="primary"
+            href="/gallery/upload"
+          >
+            사진등록&nbsp;
+            <FontAwesomeIcon icon={faPencilAlt} />
+          </Button>
+        </Container>
+        <Container>
+          <Row>
+            <Images />
+          </Row>
+        </Container>
+      </div>
     );
+  }
+}
+
+class Images extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      imageData: [
+        {
+          id: "1",
+          url:
+            "https://pgnqdrjultom1827145.cdn.ntruss.com/img/d6/3f/d63fc54819cd3fb0c319021e2e7cd6bfee951e8ce2db9e948bd828f538272da6_v1.jpg",
+        },
+        {
+          id: "2",
+          url:
+            "https://image.freepik.com/free-photo/image-human-brain_99433-298.jpg",
+        },
+      ],
+    };
+  }
+  render() {
+    return (
+      <Col xs={6} md={4}>
+        <Image
+          src={this.state.imageData.map((image) => {
+            return image.url;
+          })}
+          thumbnail
+        />
+      </Col>
+    );
+  }
 }
 
 export default GalleryPage;

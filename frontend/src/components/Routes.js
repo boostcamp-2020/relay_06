ae9231396cb3daadd52353eb331f1afe3a1faf27
Changehee Choi <changhee.dev@gmail.com>
2020-08-16 01:12:00 +0900

    fix: change a tag to NavLink in Navbar component
    a tag로 페이징을 하면 build시에 링크가 제대로 동작하지 않는 문제가 발생

--- frontend/src/components/Routes.js
+++ frontend/src/components/Routes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from '../pages/Home';
 import PostPage from '../pages/PostPage';
 import GalleryPage from '../pages/GalleryPage';
 import GalleryUploadPage from '../pages/GalleryUploadPage';
@@ -7,9 +8,10 @@ import GalleryPhotoPage from '../pages/GalleryPhotoPage';
 
 function Routes() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Switch>
-        <Route exact path="/" component={PostPage} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/diary" component={PostPage} />
         <Route exact path="/gallery" component={GalleryPage} />
         <Route exact path="/gallery/upload" component={GalleryUploadPage} />
         <Route exact path="/gallery/:id" component={GalleryPhotoPage} />

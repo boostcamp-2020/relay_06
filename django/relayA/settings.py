ddbb1660a35d761391586230a6c08d950b6d8f18
mu1616 <mu1616@naver.com>
2020-08-15 18:07:17 +0900

    update setting

--- django/relayA/settings.py
+++ django/relayA/settings.py
@@ -105,8 +105,30 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+#CORS
 CORS_ORIGIN_ALLOW_ALL=True
+CORS_ALLOW_CREDENTIALS = True
 
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+)
+
+CORS_ALLOW_HEADERS = (
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+)
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 

e5256062ca897c37902ed56b284fe6b852344c61
Changehee Choi <changhee.dev@gmail.com>
2020-08-15 13:30:53 +0900

    fix: useEffect infinite loop issue

--- frontend/src/lib/image-api.js
+++
@@ -1,6 +0,0 @@
-const getImage = async (id) => {
-  //   const result = await axios.get(`/photo/${id}`);
-  return { id: id, url: 'http://placeimg.com/640/480/any', tags: [] };
-};
-
-export { getImage };

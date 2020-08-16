36dd503d885ab91d654745b1a96337f9abf39a05
Leejoohyung-sejong <58721320+Leejoohyung-sejong@users.noreply.github.com>
2020-08-16 21:08:02 +0900

    리스트 출력

--- frontend/src/pages/GalleryPage.js
+++ frontend/src/pages/GalleryPage.js
@@ -2,64 +2,83 @@ import React, { Component } from "react";
 import Navbar from "../components/Navbar";
 import Gallery from "react-photo-gallery";
 import { photos } from "../temp/photos.js";
+import axios from '../plugin/axios';
 import { Button, Image, Container, Row, Col } from "react-bootstrap";
 import { faPencilAlt } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { imageData } from "../temp/images.js";
 
 class GalleryPage extends React.Component {
-  render() {
-    return (
-      <div>
-        <Navbar />
-        <Container>
-          <br />
-          <Button
-            style={{ float: "right" }}
-            variant="primary"
-            href="/gallery/upload"
-          >
-            사진등록&nbsp;
+
+    render() {
+        return (
+            <div>
+                <Navbar />
+                <Container>
+                    <br />
+                    <Button
+                        style={{ float: "right" }}
+                        variant="primary"
+                        href="/gallery/upload"
+                    >
+                        사진등록&nbsp;
             <FontAwesomeIcon icon={faPencilAlt} />
-          </Button>
-        </Container>
-        <br />
-        <br />
-        <br />
-        <Container>
-          <Images />
-        </Container>
-      </div>
-    );
-  }
+                    </Button>
+                </Container>
+                <br />
+                <br />
+                <br />
+                <Container>
+                    <Images />
+                </Container>
+            </div>
+        );
+    }
 }
 
 class Images extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      imageData,
-    };
-  }
-  render() {
-    return (
-      <Row>
-        {this.state.imageData.map((image, i) => {
-          return (
-            <Col xs={6} md={4}>
-              <ImageInfo id={image.id} url={image.url} key={i} />
-            </Col>
-          );
-        })}
-      </Row>
-    );
-  }
+    constructor(props) {
+        super(props);
+        
+        this.getImageInfo();
+        this.state = {
+            imageData,
+        };
+    }
+
+    getImageInfo = async () => {
+        const imageInfo = 12;
+        await axios
+          .get(`/api/photo/`)
+          .then((response) => {
+            for(let i in response.data)
+            imageData[i].url = process.env.REACT_APP_BASE_URL + response.data[i].source;
+          })
+          .catch((err) => {
+            alert('삭제된 사진이거나 요청이 잘못되었습니다.');
+            window.location = '/gallery';
+          });
+      };
+
+    render() {
+        return (
+            <Row>
+                {this.state.imageData.map((image, i) => {
+                    return (
+                        <Col xs={6} md={4}>
+                            <ImageInfo id={image.id} url={image.url} key={i} />
+                        </Col>
+                    );
+                })}
+            </Row>
+        );
+    }
 }
 
 class ImageInfo extends React.Component {
-  render() {
-    return <Image src={this.props.url} thumbnail></Image>;
-  }
+    render() {
+        return <Image src={this.props.url} thumbnail></Image>;
+    }
 }
 
 export default GalleryPage;

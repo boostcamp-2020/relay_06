7753e43140b70e0f64ec7edcd4a77ba70dd0a639
YoonWoo <lkkjasd@korea.ac.kr>
2020-08-21 16:54:18 +0900

    LoginPage axios 구현 더 해야함

--- frontend/src/components/Routes.js
+++ frontend/src/components/Routes.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from '../pages/Home';
-import PostPage from '../pages/PostPage';
-import GalleryPage from '../pages/GalleryPage';
-import GalleryUploadPage from '../pages/GalleryUploadPage';
-import GalleryPhotoPage from '../pages/GalleryPhotoPage';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "../pages/Home";
+import PostPage from "../pages/PostPage";
+import GalleryPage from "../pages/GalleryPage";
+import GalleryUploadPage from "../pages/GalleryUploadPage";
+import GalleryPhotoPage from "../pages/GalleryPhotoPage";
+import LoginPage from "../pages/LoginPage";
 
 function Routes() {
   return (
@@ -15,6 +16,7 @@ function Routes() {
         <Route exact path="/gallery" component={GalleryPage} />
         <Route exact path="/gallery/upload" component={GalleryUploadPage} />
         <Route exact path="/gallery/:id" component={GalleryPhotoPage} />
+        <Route exact path="/login" component={LoginPage} />
       </Switch>
     </Router>
   );
